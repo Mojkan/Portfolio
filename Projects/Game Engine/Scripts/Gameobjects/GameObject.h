48822de99b80c57933c356e0472bdf7e38cf0046
Mojkan <74500191+Mojkan@users.noreply.github.com>
2026-03-04 10:04:41 +0100

    Removed comments

--- Projects/Game Engine/Scripts/Gameobjects/GameObject.h
+++ Projects/Game Engine/Scripts/Gameobjects/GameObject.h
@@ -8,7 +8,6 @@
 #include "Tags.h"
 #include "RenderGroups.h"
 
-// Maximum types of different componetns
 constexpr std::size_t maxComponents = 32;
 
 class GameObjectManager;
@@ -49,51 +48,31 @@ public:
         return componentBitset[getComponentTypeID<T>()];
     }
 
-	// A template function for adding new components to an entity
-	// T is the component type, Targs lets you pass arguments to the component
-	// These ... indicates a parameter pack
-	// TArgs&& allows for named variables and temporaries
-	// Targs is for the types of the arguments and mArgs the values of the arguments
 	template <typename T, typename... TArgs>T& addComponent(TArgs&&... mArgs)
 	{
-		// T* component declares a pointer of that component type
-		// new T creates a new component on the heap
-		// mArgs are the argument values and TArgs the argument types
 		T* component(new T(std::forward<TArgs>(mArgs)...));
-		// Sets the entity to the component to this entity
 		component->gameObject = this;
-		// Makes the component pointer to a unique_ptr and takes ownership of it
 		std::unique_ptr<Component> uPtr{ component };
-		// Moves the uPtr to the vector of components pointers
 		components.emplace_back(std::move(uPtr));
 
-		// Stores the component in a array of component pointers
 		componentArray[getComponentTypeID<T>()] = component;
-		// Sets the specific component to true
 		componentBitset[getComponentTypeID<T>()] = true;
 
-		// Returns a pointer to the created component
 		return *component;
 	}
 
-	// A template that returns a reference to a specific component type
 	template<typename T> T& getComponent() const
 	{
-		// Get the stored pointer for this component type from the component array
 		auto ptr(componentArray[getComponentTypeID<T>()]);
-		// Cast the base Component* to the actual component type (T*) and return a reference
 		return *static_cast<T*>(ptr);
 	}
 
-	// Generates a unique ID
 	static std::size_t getComponentNewTypeID()
 	{
-		// This variable is static which doesn't destroy the variable when function is finished
 		static std::size_t lastID = 0u;
 		return lastID++;
 	}
 
-	// Assigns a unique fixed ID to each component type
 	template <typename T> static std::size_t getComponentTypeID() noexcept
 	{
 		static std::size_t typeID = getComponentNewTypeID();
